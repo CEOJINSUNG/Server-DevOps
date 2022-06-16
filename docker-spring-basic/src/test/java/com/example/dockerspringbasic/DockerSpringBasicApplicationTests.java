a5fac21ac62b956aff6f41dcee611faa374db0a9
CEOJINSUNG <jinsung1048@gmail.com>
2022-06-16 19:30:15 +0900

    docker-spring-basic

---
+++ docker-spring-basic/src/test/java/com/example/dockerspringbasic/DockerSpringBasicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.dockerspringbasic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerSpringBasicApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
